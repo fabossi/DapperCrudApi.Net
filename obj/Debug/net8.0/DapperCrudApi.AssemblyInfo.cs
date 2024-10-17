9c9fc7e280497f2a9d6300cbcce2a820fdc78a70
Felipe Augusto Fabossi De Oliveira Costa <felipe.fabossi@montreal.com.br>
2024-10-17 08:41:09 -0300

    Pull

--- obj/Debug/net8.0/DapperCrudApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/DapperCrudApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DapperCrudApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca422e943ee82517e73a5e44549b596f6b71f78f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+618a66c1f886bc867c5e885eaf9f603370b9e245")]
 [assembly: System.Reflection.AssemblyProductAttribute("DapperCrudApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DapperCrudApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
